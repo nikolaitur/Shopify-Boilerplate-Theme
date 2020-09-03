6e547a137831310273c63031f3f8857f5ad1db05
nikolaitur <n.turovskyi@gmail.com>
2020-09-02 23:10:22 -0600

    add search widget

--- sources/assets/javascripts/modules/search.js
+++ sources/assets/javascripts/modules/search.js
@@ -1,21 +1,18 @@
 import $ from 'jquery';
 
 export default function() {
-    //alert();
-  $('[data-popup-close]').click(function(){
-    $(this).closest('[data-popup]').addClass('is-hidden').trigger('setDefaultContent');
-  })
-
-  $('[data-popup-open]').click(function(event) {
-    event.preventDefault();
-    var popup = $(this).data('popup-open');
-    var $popup = $('[data-popup="'+ popup +'"');
-    $popup.toggleClass('is-hidden');
+  $(".search-close").click(function(){
+    $('[data-overlay-search]').removeClass('is_active');
   });
 
-  // Open popup when popup form loaded with errors
-  $('[data-popup-form-errors]').each(function (index, element) {
-    const popup = $(this).data('popup-form-errors');
-    $(`[data-popup-open="${popup}"]`).trigger('click');
+  $('[data-show-search-trigger]').click(function(){
+    var display_mode = $(this).data('search-style');
+    if(display_mode == "overlay"){
+      $('[data-overlay-search]').toggleClass('is_active');
+      $("[data-popup='search-popup']").addClass('is-hidden');
+      $('[data-search-input]').focus();
+    }else{
+      
+    }
   });
 }
